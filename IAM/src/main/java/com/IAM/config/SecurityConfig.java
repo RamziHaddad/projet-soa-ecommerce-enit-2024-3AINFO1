68ec9c7810483c9655e4b1a91a8624a72f13f828
Siwar Terres <130316042+siwar630@users.noreply.github.com>
2024-12-11 21:27:21 +0100

    Security config updated

--- IAM/src/main/java/com/IAM/config/SecurityConfig.java
+++ IAM/src/main/java/com/IAM/config/SecurityConfig.java
@@ -1,1 +1,72 @@
-s
+package com.IAM.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.client.RestTemplate;
+
+@KeycloakConfiguration
+public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/api/keycloak/auth/login",
+            "/api/keycloak/auth/register",
+    };
+    private static final String ISSUER_URI = "http://92.222.24.59:8080/realms/ecommerce-platform";
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(ISSUER_URI);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    protected DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt ->
+                        jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/ecommerce/**").hasRole("ECOMMERCE_ADMIN")
+                        .requestMatchers("/api/manager/**").hasRole("ECOMMERCE_MANAGER")
+                        .requestMatchers("/api/user/**").hasRole("ECOMMERCE_USER")
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        // Set a custom GrantedAuthorities converter
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return jwtAuthenticationConverter;
+    }
+
+
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+}
---
+++ IAM/src/main/java/com/IAM/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.IAM.exceptions;
+
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleAccessDeniedException(AccessDeniedException ex) {
+        return "Access Denied: " + ex.getMessage();
+    }
+}
