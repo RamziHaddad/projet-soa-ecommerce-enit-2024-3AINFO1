5518d41c904614dd50915c6df0707309985d25ea
Siwar Terres <130316042+siwar630@users.noreply.github.com>
2024-12-12 21:19:59 +0100

    GlobalExceptionHandler Updated

--- IAM/src/main/java/com/IAM/exceptions/GlobalExceptionHandler.java
+++ IAM/src/main/java/com/IAM/exceptions/GlobalExceptionHandler.java
@@ -1,18 +1,23 @@
 package com.IAM.exceptions;
 
-
-import org.springframework.security.access.AccessDeniedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(AccessDeniedException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public String handleAccessDeniedException(AccessDeniedException ex) {
-        return "Access Denied: " + ex.getMessage();
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Une erreur est survenue : " + ex.getMessage());
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Erreur de validation : " + ex.getMessage());
+    }
+
 }
