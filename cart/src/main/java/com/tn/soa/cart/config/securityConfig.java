36974a5cd415bef09b2772d8d715b08065d1b8a5
rayray252 <rayen.meddah@etudiant-enit.utm.tn>
2024-12-29 17:06:31 +0100

    keycloak integration

---
+++ cart/src/main/java/com/tn/soa/cart/config/securityConfig.java
@@ -0,0 +1,67 @@
+package com.tn.soa.cart.config;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import
+        org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticatio
+nConverter;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import
+        org.springframework.security.web.authentication.session.RegisterSessionAuthentica
+tionStrategy;
+import
+        org.springframework.security.web.authentication.session.SessionAuthenticationStrat
+egy;
+import org.springframework.web.client.RestTemplate;
+@KeycloakConfiguration
+public class SecurityConfig {
+    private static final String ISSUER_URI =
+            "http://92.222.24.59:8080/realms/ecommerce-platform";
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation(ISSUER_URI);
+    }
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+    @Bean
+    protected DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt ->
+                        jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/cart/{customerId}").hasAnyRole("ECOMMERCE_ADMIN", "ECOMMERCE_MANAGER", "ECOMMERCE_USER")
+                        .requestMatchers("/api/cart/create").hasAnyRole("ECOMMERCE_ADMIN", "ECOMMERCE_MANAGER")
+                        .requestMatchers("/api/cart/{customerId}/add").hasAnyRole("ECOMMERCE_ADMIN", "ECOMMERCE_MANAGER")
+                        .requestMatchers("/api/cart/{customerId}/remove/{itemId}").hasAnyRole("ECOMMERCE_ADMIN", "ECOMMERCE_MANAGER")
+                        .requestMatchers("/api/cart/{customerId}/clear").hasAnyRole("ECOMMERCE_ADMIN", "ECOMMERCE_MANAGER")
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new
+                JwtAuthenticationConverter();
+// Set a custom GrantedAuthorities converter
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new
+                KeycloakRoleConverter());
+        return jwtAuthenticationConverter;
+    }
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+}
